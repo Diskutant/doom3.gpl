7c52572b15ebf3262ad628d84ff06d39395b3e97
Diskutant <diskutant+git@googlemail.com>
2012-07-14 19:10:01 +0200

    Remove other unneeded defines

--- neo/framework/BuildDefines.h
+++ neo/framework/BuildDefines.h
@@ -87,11 +87,8 @@ If you have questions concerning this license or the applicable additional terms
 // compiled out.
 //#define ID_DEDICATED
 
-// if this is defined, the executable positively won't work with any paks other
-// than the demo pak, even if productid is present.
-//#define ID_DEMO_BUILD
 
-// don't define ID_ALLOW_TOOLS when we don't want tool code in the executable.
+	// don't define ID_ALLOW_TOOLS when we don't want tool code in the executable.
 #if defined( _WIN32 ) && !defined( ID_DEDICATED ) && !defined( ID_DEMO_BUILD )
 	#define	ID_ALLOW_TOOLS
 #endif
